a1c3673ffb994a134eed2730a8f8a79055fe4b4b
wasim <wasim123@gmail.com>
2023-10-02 12:55:40 +0530

    Second Client

---
+++ src/main/java/com/EurekaClient2Example/EurekaClient2/controller/Eurekaclient2Controller.java
@@ -0,0 +1,63 @@
+package com.EurekaClient2Example.EurekaClient2.controller;
+
+import com.EurekaClient2Example.EurekaClient2.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.core.env.Environment;
+
+
+@RestController
+@RequestMapping("/consumer")
+public class Eurekaclient2Controller {
+    @Autowired
+    Environment environment;
+    private static List productList = new ArrayList<>();
+    static{
+        productList = new ArrayList<>();
+        Product p1 = new Product(UUID.randomUUID(), "Guitar" , 99.99, "Music");
+        Product p2 = new Product(UUID.randomUUID(), "AWS Book" , 29.99, "Books");
+        Product p3 = new Product(UUID.randomUUID(), "Bread" , 9.99, "Food");
+        Product p4 = new Product(UUID.randomUUID(), "Soap" , 99.99, "Body");
+        Product p5 = new Product(UUID.randomUUID(), "Milk" , 3.99, "Food");
+        Product p6 = new Product(UUID.randomUUID(), "Pant" , 17.99, " Cloth");
+        Product p7 = new Product(UUID.randomUUID(), "Tyre" , 39.99, "Auto");
+        productList.add(p1);
+        productList.add(p2);
+        productList.add(p3);
+        productList.add(p4);
+        productList.add(p5);
+        productList.add(p6);
+        productList.add(p7);
+    }
+
+    @GetMapping("/calleurekaclient2")
+    public ResponseEntity callEurekaClient2(){
+        return new ResponseEntity("Hello From Client 2 From port No. "+environment.getProperty("local.server.port"), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/allproducts", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List list() {
+        return Eurekaclient2Controller.productList;
+    }
+
+    @GetMapping(value = "/product/{id}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product showProduct(@PathVariable UUID id) {
+        return new Product(id, "Guitar" , 99.99, "Music");
+    }
+
+    @PostMapping(value = "/product/update")
+    public String saveProduct(@RequestBody Product product) {
+        return "Product with product id: "+ product.getProductId() +" and product name:"+product.getProductName()+" has been saved successfully!";
+    }
+
+    @DeleteMapping(value = "/product/delete/{id}")
+    public void delete(@PathVariable UUID id) {
+        //log "Product "+ id +" has been deleted successfully!";
+    }
+}
